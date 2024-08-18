de413f133167ada0558b5306ec6c25dd7e765e43
Pratyush-Neupane <pratyush3naupane@gmail.com>
2024-08-18 09:17:47 +0545

    Layout completed.

---
+++ websites/default/public/dbconnection.php
@@ -0,0 +1,12 @@
+$servername = "mysql";
+$username = "root";
+$password = "root";
+$database = "wucdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
---
+++ websites/default/public/nav.php
@@ -0,0 +1,21 @@
+<!-- 
+    <link rel="stylesheet" href="style.css"> 
+
+<header>
+    <div class="logo">
+           <img src="WUC.png" alt="University Logo">
+    </div>
+    <nav>
+        <ul>
+            <li><a href="index.php" class="<?= basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : '' ?>">Home</a></li>
+            <li><a href="academics.php" class="<?= basename($_SERVER['PHP_SELF']) == 'academics.php' ? 'active' : '' ?>">Academics</a></li>
+            <li><a href="students.php" class="<?= basename($_SERVER['PHP_SELF']) == 'students.php' ? 'active' : '' ?>">Students</a></li>
+            <li><a href="faculty.php" class="<?= basename($_SERVER['PHP_SELF']) == 'faculty.php' ? 'active' : '' ?>">Faculty</a></li>
+            <li><a href="contact.php" class="<?= basename($_SERVER['PHP_SELF']) == 'contact.php' ? 'active' : '' ?>">Contact</a></li>
+            <li><a href="#" class="login">LOGIN</a></li>
+        </ul>
+        <a href="#" class="login"><Button>Login</Button></a>
+
+        
+    </nav>
+</header> -->
