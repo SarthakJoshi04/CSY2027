d26ba4bd539e4314168f6c9030a110d9acd8119d
Pratyush-Neupane <pratyush3naupane@gmail.com>
2024-08-31 13:07:27 +0545

    finished

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
--- Adminer 4.8.1 MySQL 11.2.2-MariaDB-1:11.2.2+maria~ubu2204 dump
+-- Adminer 4.8.1 MySQL 11.4.3-MariaDB-ubu2404 dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -14,11 +14,15 @@ CREATE TABLE `assignments` (
   `description` text DEFAULT NULL,
   `due_date` date DEFAULT NULL,
   `course_id` int(11) DEFAULT NULL,
+  `file_path` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `course_id` (`course_id`),
   CONSTRAINT `assignments_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+INSERT INTO `assignments` (`id`, `title`, `description`, `due_date`, `course_id`, `file_path`) VALUES
+(34,	'Java Application',	'This is the assignment',	'2024-08-20',	4,	'Nami Project Introduction page.docx'),
+(37,	'demo',	'demo',	'2024-08-29',	4,	'hello.docx');
 
 DROP TABLE IF EXISTS `attendance`;
 CREATE TABLE `attendance` (
@@ -44,6 +48,24 @@ CREATE TABLE `courses` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+INSERT INTO `courses` (`id`, `course_name`, `description`, `is_archived`) VALUES
+(4,	'BscIT',	'BSc IT full form is Bachelor of Science in IT. BSc in IT is related to storage, processing, securing, and managing information. This course primarily focuses on subjects such as databases, software and networking.',	0),
+(6,	'Data Science',	'This course provides a comprehensive overview of the data science field, covering key concepts such as data collection, data cleaning, exploratory data analysis, and statistical modeling.',	0);
+
+DROP TABLE IF EXISTS `grades`;
+CREATE TABLE `grades` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `grade` decimal(5,2) NOT NULL,
+  `date_assigned` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `student_id` (`student_id`),
+  KEY `course_id` (`course_id`),
+  CONSTRAINT `grades_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `grades_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
 
 DROP TABLE IF EXISTS `modules`;
 CREATE TABLE `modules` (
@@ -54,9 +76,12 @@ CREATE TABLE `modules` (
   `is_archived` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `course_id` (`course_id`),
-  CONSTRAINT `modules_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
+  CONSTRAINT `fk_course_id` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+INSERT INTO `modules` (`id`, `module_name`, `description`, `course_id`, `is_archived`) VALUES
+(7,	'System Design and Development',	'The courses in this certificate focus on teaching students engineering design and management of large complex systems, including software-intensive systems.',	4,	0),
+(8,	'SADSAMDS',	'ASDSAD',	4,	1);
 
 DROP TABLE IF EXISTS `staff`;
 CREATE TABLE `staff` (
@@ -72,6 +97,8 @@ CREATE TABLE `staff` (
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
+INSERT INTO `staff` (`id`, `firstname`, `lastname`, `email`, `username`, `password`, `is_archived`) VALUES
+(7,	'Ram',	'Khadka',	'ram@gmail.com',	'ram',	'$2y$10$GZM8frEb3HtarMU2ndFDvuLxr1PKEp1j.yvieT5Guc.jfbO/dl3KG',	0);
 
 DROP TABLE IF EXISTS `students`;
 CREATE TABLE `students` (
@@ -82,10 +109,54 @@ CREATE TABLE `students` (
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `is_archived` tinyint(1) DEFAULT 0,
+  `course_id` int(11) DEFAULT NULL,
+  `module_id` int(11) DEFAULT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `contact` varchar(20) DEFAULT NULL,
+  `address` text DEFAULT NULL,
+  `parents` text DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `username` (`username`)
+  UNIQUE KEY `username` (`username`),
+  KEY `course_id` (`course_id`),
+  KEY `module_id` (`module_id`),
+  CONSTRAINT `students_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`),
+  CONSTRAINT `students_ibfk_2` FOREIGN KEY (`module_id`) REFERENCES `modules` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+INSERT INTO `students` (`id`, `firstname`, `lastname`, `email`, `username`, `password`, `is_archived`, `course_id`, `module_id`, `date_of_birth`, `gender`, `contact`, `address`, `parents`) VALUES
+(10,	'Jhon',	'Doe',	'Jhon@gmail.com',	'jhon',	'$2y$10$t5fcxW5m5dr/fqUeoP1o9OqxrUMTDm8Fx0qo.uQbm.5TRUN1THl/K',	0,	4,	7,	'2024-08-21',	'Male',	'9876153452',	'Nepal, kathmandu',	'jenny'),
+(12,	'Henry',	'Park',	'Henry@gmail.com',	'Henry',	'$2y$10$dnhfp2WQxThZPM9QuyLi2O/iyVzQBsMR3xVFOjUYAQBPQU/lHVEae',	0,	4,	7,	'2019-11-05',	'Male',	'987618744',	'new york',	'peter');
+
+DROP TABLE IF EXISTS `student_courses`;
+CREATE TABLE `student_courses` (
+  `student_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  PRIMARY KEY (`student_id`,`course_id`),
+  KEY `course_id` (`course_id`),
+  CONSTRAINT `student_courses_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `student_courses_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 
--- 2024-08-24 21:06:26
+DROP TABLE IF EXISTS `submissions`;
+CREATE TABLE `submissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `assignment_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `submission_date` timestamp NULL DEFAULT current_timestamp(),
+  `student_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `assignment_id` (`assignment_id`),
+  KEY `student_id` (`student_id`),
+  CONSTRAINT `submissions_ibfk_1` FOREIGN KEY (`assignment_id`) REFERENCES `assignments` (`id`),
+  CONSTRAINT `submissions_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+INSERT INTO `submissions` (`id`, `assignment_id`, `student_id`, `file_path`, `submission_date`, `student_name`) VALUES
+(4,	34,	10,	'hello.docx',	'2024-08-27 02:56:00',	'jhon'),
+(5,	37,	10,	'Presentation Speaker Notes (4).docx',	'2024-08-31 05:53:44',	'jhon');
+
+-- 2024-08-31 07:16:03
